f25f8832e8cff50352d0595f00b86249314d795b
Bruno Ribeiro <bruno.ribeiro.2k@outlook.com>
2021-12-19 12:30:02 +0000

    Clean code in chapter 2

--- src/main/scala/org/brunoribeiro2k/fpinscala/Chapter2.scala
+++ src/main/scala/org/brunoribeiro2k/fpinscala/Chapter2.scala
@@ -2,15 +2,7 @@ package org.brunoribeiro2k.fpinscala
 
 class Chapter2 {
 
-  def main(args: Array[String]): Unit = {
-    ???
-  }
-
-  /**
-   * Exercise 2.1: Fibonacci
-   * @param n nth Fibonacci number
-   * @return
-   */
+  /** Exercise 2.1: Fibonacci */
   def fib(n: Int): Int = {
     @annotation.tailrec
     def loop(n: Int, prev: Int, cur: Int): Int = {
@@ -20,11 +12,7 @@ class Chapter2 {
     loop(n, 0, 1)
   }
 
-  /**
-   * Exercise 2.1 without tailrec
-   * @param n nth Fibonacci number
-   * @return
-   */
+  /** Exercise 2.1 without tailrec */
   def fibNonTailrec(n: Int): Int = {
     n match {
       case x if x <= 0 => 0
@@ -33,13 +21,7 @@ class Chapter2 {
     }
   }
 
-  /**
-   * Exercise 2.2
-   * @param as
-   * @param ordered
-   * @tparam A
-   * @return
-   */
+  /** Exercise 2.2 */
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @annotation.tailrec
     def loop(n: Int): Boolean = {
@@ -51,39 +33,17 @@ class Chapter2 {
     loop(0)
   }
 
-  /**
-   * Exercise 2.3
-   * @param f
-   * @tparam A
-   * @tparam B
-   * @tparam C
-   * @return
-   */
+  /** Exercise 2.3 */
   def curry[A,B,C](f: (A, B) => C): A => B => C = {
     a => f(a, _)
   }
 
-  /**
-   * Exercise 2.4
-   * @param f
-   * @tparam A
-   * @tparam B
-   * @tparam C
-   * @return
-   */
+  /** Exercise 2.4 */
   def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
     (a, b) => f(a)(b)
   }
 
-  /**
-   * Exercise 2.5
-   * @param f
-   * @param g
-   * @tparam A
-   * @tparam B
-   * @tparam C
-   * @return
-   */
+  /** Exercise 2.5 */
   def compose[A,B,C](f: B => C, g: A => B): A => C = {
     (a: A) => f(g(a))
     // g andThen f
